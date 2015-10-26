c85a897cbc97c11bbf16758de5f5911d6a3a4877
normgiff <agiffiv@gmail.com>
2015-10-26 14:11:57 -0600

    adding the start of the imput

---
+++ io/input.c
@@ -0,0 +1,105 @@
+#include <string.h>
+#include <stdio.h>
+
+typedef struct sizes
+{
+	int lines;
+	int chars;
+} sizes_t;
+
+
+
+
+sizes_t line_number(char *file)
+{
+	sizes_t numbers;
+	int lines;
+	int max_char = 0;
+	int len = 0;
+	FILE *f = fopen(file,"r");
+	
+	if (f == NULL)
+  		return numbers;
+  	while(!feof(f))
+    {
+    	if(fgetc(f) == '\n')
+    	{
+    		if(len >= max_char)
+    			max_char = len;
+    		len = 0;
+    		lines++;
+    	}
+    	else
+    	{
+    		len++;
+    	}	
+    }
+    numbers.lines = lines+1;
+    numbers.chars = max_char;
+    fclose(f);
+	return numbers;
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+	char c;
+    char *input = argv[1];
+    FILE *input_file;
+	sizes_t line_num = line_number(input);
+	int num_lines = line_num.lines;
+	int max_char = line_num.chars;
+	
+	char *lines[8];
+	//char *lines[num_lines];
+    char line[max_char];
+    
+    input_file = fopen(input, "r");
+    
+    
+    if (input_file == 0)
+    {
+        //fopen returns 0, the NULL pointer, on failure
+        perror("Canot open input file\n");
+     	return -1;
+    }
+    
+    
+   
+    
+    printf("The number of lines in the file: %i and chars: %i\n", num_lines, max_char);
+    int j = 0;
+    int i = 0;
+    do 
+    {
+    
+    	c = fgetc(input_file);
+    	//printf("Characters in the string.: %c\n", c);
+    	if(c == '\n')
+    	{
+			j = 0;
+    		printf("Line %i: %s\n", i, line);
+    		int n;
+    		//char line[max_char];
+    		//lines[i] = line;
+			i++;
+    					
+		}
+		else
+    	{
+    		//printf("Characters in the string.: %c, %i\n", c, j);
+    		line[j] = c;
+    		j++;
+    		
+    		
+    		
+    	}
+    		
+
+    } while (!feof(input_file));
+
+	fclose(input_file);
+	return 0;
+}
