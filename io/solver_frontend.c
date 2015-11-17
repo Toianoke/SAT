50b5fef70459c1e6c65c93543b77706379844086
Ian Briggs <ian.briggs@utah.edu>
2015-11-17 14:53:09 -0700

    added todo statements and UNKNOWN output

--- io/solver_frontend.c
+++ io/solver_frontend.c
@@ -20,10 +20,24 @@ error_on(int bool)
   }
 }
 
+
 /**
- * Similar interface as strtol without base, 
+ * Prints "UNKNOWN\n" and exits with 0 condition when bool is true
+ */
+void
+unknown_on(int bool)
+{
+  if (bool) {
+    printf("UNKNOWN\n");
+    exit(0);
+  }
+}
+
+
+/**
+ * Similar interface as strtol without base,
  *     and with inclusive min and max values
- * Exits on all non-parsableor invalid inputs
+ * Exits on all non-parsable or invalid inputs
  */
 long
 parse_long(char* str, char ** endptr, long min, long max)
@@ -35,6 +49,7 @@ parse_long(char* str, char ** endptr, long min, long max)
   error_on(retval > max);
   return retval;
 }
+
 
 /**
  * Opens file and reads the reduced DIMACS header.
@@ -48,7 +63,7 @@ read_header(char * filename, short * nbvar, short * nbclauses)
   error_on(fp == NULL);
 
   char * buff = (char *) malloc(sizeof(char)*(BUFFSIZE+1));
-  error_on(fp == NULL);
+  unknown_on(fp == NULL);
 
   while(1){
     char * out = fgets(buff, BUFFSIZE, fp);
@@ -65,12 +80,16 @@ read_header(char * filename, short * nbvar, short * nbclauses)
       continue;
     }
 
+    // TODO: skip blank lines
+
     // If its not a comment it MUST be the header line
+    // TODO: What about whitespace, eg "p  cnf"?
     error_on(strncmp("p cnf ", out, 6) != 0);
 
+    // TODO: What about whitespace causing this to split a buffer?
     char * endptr1;
     *nbvar = parse_long(&(out[6]), &endptr1, 1, MAX_VAR_CNT-1);
-    
+
     char * endptr2;
     *nbclauses = parse_long(endptr1, &endptr2, 1, MAX_CLAUSE_CNT-1);
 
@@ -79,26 +98,32 @@ read_header(char * filename, short * nbvar, short * nbclauses)
       endptr2++;
     }
     error_on(*endptr2 != '\n');
-    
+
     free(buff);
     return fp;
   }
 }
 
+
+/**
+ * Uses open file to read a DIMACS body.
+ *     Disregards comment lines.
+ * Exits on improper formatting or file errors
+ */
 short **
 read_body(FILE* fp, short nbvar, short nbclauses)
 {
   short ** retval = (short **) malloc(sizeof(short *)*(nbclauses));
-  error_on(retval == NULL);
+  unknown_on(retval == NULL);
 
   short * temp = (short *) malloc(sizeof(short)*(nbvar+1));
-  error_on(temp == NULL);
-  
+  unknown_on(temp == NULL);
+
   char * buff = (char *) malloc(sizeof(char)*(BUFFSIZE+1));
-  error_on(buff == NULL);
+  unknown_on(buff == NULL);
 
   int clause_index = 0;
-  temp[0] = 0; // each clause starts with an implicit 0 for use by the solver
+  temp[0] = 0;
   char * out;
   while (1) {
     out = fgets(buff, BUFFSIZE, fp);
@@ -114,6 +139,8 @@ read_body(FILE* fp, short nbvar, short nbclauses)
       continue;
     }
 
+    // TODO: skip empty lines
+
     // place numbers into temp, error on same or negate literals in a line
     char * endptr1 = out;
     char * endptr2;
@@ -127,13 +154,15 @@ read_body(FILE* fp, short nbvar, short nbclauses)
 	error_on(temp[j] == temp[i]);
 	error_on(temp[j] == -temp[i]);
       }
-      
+
       endptr1 = endptr2;
       if (temp[i] == 0) {
 	found_zero = 1;
 	break;
       }
     }
+
+    // TODO: what if a line splits a buffer?
 
     // a 0 must be the last thing in a line other than whitespace
     error_on(found_zero == 0);
@@ -144,16 +173,17 @@ read_body(FILE* fp, short nbvar, short nbclauses)
 
     // malloc the final placement of the array and copy from temp
     retval[clause_index] = (short *) malloc(sizeof(short)*(i));
-    error_on(retval[clause_index] == NULL);
+    unknown_on(retval[clause_index] == NULL);
     memcpy(retval[clause_index], temp, sizeof(short)*(i+1));
 
-    // Go to next clause and null terminate when done
+    // Go to next clause and terminate when done
     clause_index++;
     if (clause_index == nbclauses) {
       // there should be no lines with content after the last clause;
       // read until there is no more
       out = endptr2;
       while (!feof(fp)) {
+	// TODO skip comments
 	while (*out == ' ') {
 	  out ++;
 	}
@@ -161,7 +191,7 @@ read_body(FILE* fp, short nbvar, short nbclauses)
 	out = fgets(buff, BUFFSIZE, fp);
 	error_on(ferror(fp) != 0);
       }
-      
+
       free(temp);
       free(buff);
       return retval;
@@ -169,6 +199,15 @@ read_body(FILE* fp, short nbvar, short nbclauses)
   }
 }
 
+
+/**
+ * Takes a single filename as input.
+ * Prints ERROR\n on invalid input: no filename given, invalid filename,
+ *                                  improperly formatted file,
+ * Prints UNKNOWN\n on internal error: malloc failing
+ * Prints SATISFIABLE\n if input is sat
+ * Prints UNSATISFIABLE\n if input is unsat
+ */
 int
 main(int argc, char ** argv)
 {
@@ -180,6 +219,7 @@ main(int argc, char ** argv)
   short ** clauses = read_body(fp, nbvar, nbclauses);
   fclose(fp);
 
+#ifdef DEBUG
   printf("The final array with %d clauses and at the most %d "
 	 "variables in each clause:\n", nbclauses, nbvar);
   short i,j;
@@ -192,7 +232,8 @@ main(int argc, char ** argv)
     }
     printf("\n");
   }
-  
+#endif
+
   if (solve(nbvar, nbclauses, clauses)) {
     printf("SATISFIABLE\n");
   } else {
